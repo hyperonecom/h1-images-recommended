84cc1a966434e6b176f8e67c760005aba5163e01
Adam Dobrawy <naczelnik@jawnosc.tk>
2019-02-10 03:52:09 +0100

    Add arp in fedora-28

--- scripts/fedora/fedora-28.sh
+++ scripts/fedora/fedora-28.sh
@@ -28,3 +28,5 @@ touch .autorelabel
 # We quietly ignore errors, because we have to fix the main problems, which is difficult in chroot. 
 # The rest of the changes will be made during the virtual machine start.
 fixfiles -F relabel || true 
+echo "net.ipv4.conf.all.arp_notify = 1" >> /etc/sysctl.d/gratuitous-arp.conf
+echo "net.ipv4.conf.default.arp_notify = 1" >> /etc/sysctl.d/gratuitous-arp.conf
