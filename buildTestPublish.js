d9dc2afeaa15da49eaab254bcb0c3898178c61af
Adam Dobrawy <naczelnik@jawnosc.tk>
2019-02-25 11:59:12 +0100

    Move cleanup to buildTestPublish

--- buildTestPublish.js
+++ buildTestPublish.js
@@ -5,9 +5,19 @@ const setScope = require('./lib/setScope');
 const HyperOneApi = require('hyper_one_api');
 const defaultClient = HyperOneApi.ApiClient.instance;
 setScope(defaultClient, process.env);
-const apiInstance = new HyperOneApi.ImageApi();
+
+defaultClient.defaultHeaders = {
+    'Prefer':`respond-async,wait=${60 * 60 * 24}`
+};
+
+const imageApi = new HyperOneApi.ImageApi();
+const vmApi = new HyperOneApi.VmApi();
+const diskApi = new HyperOneApi.DiskApi();
 
 const scopeActive = (process.env.SCOPE || 'h1').toLowerCase();
+
+const olderThan = (resource, ageInMinutes) => new Date(image.createdOn) < (new Date() - ageInMinutes * 60 * 1000);
+const state = states => resource => states.includes(resource.state);
 
 const config = {
     rbx: {
@@ -54,13 +64,39 @@ const testImage = (imageId) => runProcess('./run_tests.sh', [
     H1_TOKEN: defaultClient.authentications.ServiceAccount.apiKey,
 });
 
-const publishImage = (imageId) => apiInstance.imagePostAccessrights(imageId, HyperOneApi.ImagePostAccessrights.constructFromObject({identity: '*'}));
+const publishImage = (imageId) => imageApi.imagePostAccessrights(imageId, HyperOneApi.ImagePostAccessrights.constructFromObject({identity: '*'}));
+
+const cleanupImage = async () => {
+    const images = await imageApi.imageList();
+    for (const image of images.filter(image => olderThan(image, 3 * 24 * 60)) && image.name.includes('image-builder')) {
+        await imageApi.imageDelete(image._id);
+    }
+};
+
+const cleanupVm = async () => {
+    const vms = await vmApi.vmList();
+    for (const vm of vms.filter(image => olderThan(image, 90))) {
+        await vmApi.vmDelete(vm._id, new HyperOneApi.VmDelete());
+    }
+};
+
+const cleanupDisk = async () => {
+    const disks = await diskApi.diskList();
+    for (const vm of disks.filter(state(['Detached']))) {
+        await diskApi.diskDelete(vm._id);
+    }
+};
 
 const main = async (template_file) => {
-    console.log({template_file});
-    const imageId = await buildImage(template_file);
-    await testImage(imageId);
-    await publishImage(imageId);
+    try {
+        const imageId = await buildImage(template_file);
+        await testImage(imageId);
+        await publishImage(imageId);
+    } finally {
+        await cleanupImage();
+        await cleanupVm();
+        await cleanupDisk();
+    }
 };
 
 main(process.argv[2]).then(console.log).catch(err => {
--- buildTravis.sh
+++ buildTravis.sh
@@ -4,5 +4,3 @@ cp ./resources/secrets/id_rsa.pub ./resources/ssh/id_rsa.pub;
 docker build . -t recommended-images;
 COMMON="-e H1_TOKEN=$H1_TOKEN -e RBX_TOKEN=$RBX_TOKEN -e SCOPE=$SCOPE";
 docker run --rm -it $COMMON recommended-images nodejs buildTestPublish.js "$TEMPLATE"
-docker run --rm -it $COMMON recommended-images nodejs cleanupImage.js;
-docker run --rm -it $COMMON recommended-images nodejs cleanupVm.js;
