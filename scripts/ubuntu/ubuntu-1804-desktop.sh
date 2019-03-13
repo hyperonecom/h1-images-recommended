26aad5501fde2cba2c30be891e48bade9126b151
Adam Dobrawy <naczelnik@jawnosc.tk>
2019-03-13 01:15:32 +0100

    Avoid non-required & keep arping on Ubuntu 18.04 Desktop

--- scripts/ubuntu/ubuntu-1804-desktop.sh
+++ scripts/ubuntu/ubuntu-1804-desktop.sh
@@ -1,3 +1,3 @@
-apt-get install -y ubuntu-desktop
+apt-get install -y ubuntu-desktop arping --no-install-recommends
 rm /etc/network/if-pre-up.d/wpasupplicant || echo "No wpasupplicant file"
 systemctl set-default graphical
