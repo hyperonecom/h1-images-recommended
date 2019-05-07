6bda7b7548965444982e10a2efbfae54d038aac5
Adam Dobrawy <naczelnik@jawnosc.tk>
2019-05-07 19:19:41 +0200

    Add missing cleanup in buildTravis.sh

--- buildTravis.sh
+++ buildTravis.sh
@@ -3,4 +3,4 @@ set -eux
 cp ./resources/secrets/id_rsa.pub ./resources/ssh/id_rsa.pub;
 docker build . -t recommended-images;
 COMMON="-e H1_TOKEN=$H1_TOKEN -e RBX_TOKEN=$RBX_TOKEN -e SCOPE=$SCOPE";
-docker run --rm -it $COMMON recommended-images nodejs buildTestPublish.js --config "$CONFIG" --mode "$MODE"
+docker run --rm -it $COMMON recommended-images nodejs buildTestPublish.js --config "$CONFIG" --mode "$MODE" --cleanup
