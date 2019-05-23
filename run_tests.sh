3108df3eecf13294d5e014bffd62a972ab0fe8d5
Adam Dobrawy <naczelnik@jawnosc.tk>
2019-05-23 12:04:10 +0200

    Update run_tests.sh delay

--- run_tests.sh
+++ run_tests.sh
@@ -77,7 +77,7 @@ VM_DISK_ID=$(${RBX_CLI} vm disk list --vm $VM_ID --output tsv --query "[].{disk:
 RBX_CLI="$RBX_CLI" VM_ID="$VM_ID" IMAGE_ID="$IMAGE" USER="$USER" IP="$EXTERNAL_IP" HOSTNAME="$VM_NAME" bats "./tests/common.bats"
 
 if [ "$os" == "packer" ]; then
-	for i in {1..60}; do echo -n '.'; sleep 1; done; echo "";
+	for i in {1..300}; do echo -n '.'; sleep 1; done; echo "";
 	${RBX_CLI} vm serialport log --vm "$VM_ID";
 	ping -c 3 "$VM_IP";
 	RBX_CLI="$RBX_CLI" USER="$USER" IP="$EXTERNAL_IP" HOSTNAME="$VM_NAME" bats "./tests/${os}.bats"
