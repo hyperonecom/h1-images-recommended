8952c593c2af0a8a925baa68782b3d503a1f6ead
Adam Dobrawy <naczelnik@jawnosc.tk>
2019-08-01 10:05:30 +0200

    Fix gnupg2 for CLI installation script

--- scripts/ubuntu/cli.sh
+++ scripts/ubuntu/cli.sh
@@ -2,7 +2,7 @@
 set -eux
 SCOPE_CODE=$(echo "${SCOPE_NAME}" | tr '[:upper:]' '[:lower:]');
 . /etc/os-release;
-apt-get install gnupg2
+apt-get install -y gnupg2
 curl -fsSL "${REPOSITORY}/gpg.public.txt" | apt-key add -
 echo "deb [arch=amd64] ${REPOSITORY}/linux/${ID} ${VERSION_CODENAME} stable" > "/etc/apt/sources.list.d/${SCOPE_CODE}.list"
 apt-get update
