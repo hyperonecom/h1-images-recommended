1341943c29fbb52f05b156daec902a7b2b6b2679
Adam Dobrawy <naczelnik@jawnosc.tk>
2019-08-01 04:02:01 +0200

    Update gnupg2 for CLI installation script

--- scripts/debian/cli.sh
+++ scripts/debian/cli.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 SCOPE_CODE=$(echo "${SCOPE_NAME}" | tr '[:upper:]' '[:lower:]');
 . /etc/os-release
+apt-get install gnupg2
 VERSION_CODENAME=$(sed -E -n 's/VERSION=.*\((.+?)\).*$/\1/gp' /etc/os-release)
-curl -fsSL "https://${REPOSITORY}/gpg.public.txt" | apt-key add -
-echo "deb [arch=amd64] ${REPOSITORY}/linux/${ID} ${VERSION_CODENAME} stable" > /etc/apt/sources.list.d/${SCOPE_CODE}.list
+curl -fsSL "${REPOSITORY}/gpg.public.txt" | apt-key add -
+echo "deb [arch=amd64] ${REPOSITORY}/linux/${ID} ${VERSION_CODENAME} stable" > "/etc/apt/sources.list.d/${SCOPE_CODE}.list"
 apt-get update
 apt-get install -y "${CLI_PACKAGE}"
--- scripts/ubuntu/cli.sh
+++ scripts/ubuntu/cli.sh
@@ -2,7 +2,8 @@
 set -eux
 SCOPE_CODE=$(echo "${SCOPE_NAME}" | tr '[:upper:]' '[:lower:]');
 . /etc/os-release;
+apt-get install gnupg2
 curl -fsSL "${REPOSITORY}/gpg.public.txt" | apt-key add -
-echo "deb [arch=amd64] ${REPOSITORY}/linux/${ID} ${VERSION_CODENAME} stable" > /etc/apt/sources.list.d/${SCOPE_CODE}.list
+echo "deb [arch=amd64] ${REPOSITORY}/linux/${ID} ${VERSION_CODENAME} stable" > "/etc/apt/sources.list.d/${SCOPE_CODE}.list"
 apt-get update
 apt-get install -y "${CLI_PACKAGE}"
