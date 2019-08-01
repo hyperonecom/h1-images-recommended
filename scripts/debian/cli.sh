660ea2508208900a687d12a6df5acca393c0e60c
Adam Dobrawy <naczelnik@jawnosc.tk>
2019-08-01 16:40:16 +0200

    Add apt-transport-https for Debian 8

--- scripts/debian/cli.sh
+++ scripts/debian/cli.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 SCOPE_CODE=$(echo "${SCOPE_NAME}" | tr '[:upper:]' '[:lower:]');
 . /etc/os-release
-apt-get install -y gnupg2
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg2;
 VERSION_CODENAME=$(sed -E -n 's/VERSION=.*\((.+?)\).*$/\1/gp' /etc/os-release)
 curl -fsSL "${REPOSITORY}/gpg.public.txt" | apt-key add -
 echo "deb [arch=amd64] ${REPOSITORY}/linux/${ID} ${VERSION_CODENAME} stable" > "/etc/apt/sources.list.d/${SCOPE_CODE}.list"
