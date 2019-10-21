fd57c97650f171dd85e953a941c2714ffacb920f
Adam Dobrawy <naczelnik@jawnosc.tk>
2019-10-21 15:37:37 +0200

    Add checksum of ssh-keys

--- installTravis.sh
+++ installTravis.sh
@@ -3,15 +3,14 @@ set -eux
 ENCRYPT_KEY=$1
 # Manage secrets
 echo "${ENCRYPT_KEY}" | gpg --passphrase-fd 0 --decrypt ./resources/secrets/id_rsa.gpg;
-
 # Install secrets ssh keys
 rm ./resources/ssh/id_rsa*;
 cp ./resources/secrets/id_rsa* ./resources/ssh/;
+md5sum resources/ssh/*
 
 # Install Docker
 # Pre-installed Docker doesn't support multi-stage builds
 # See https://github.com/travis-ci/travis-ci/issues/8181
-lsb_release -cs;
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -;
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable";
 sudo apt-get update;
