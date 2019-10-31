1202f9963826cd60fe48d0963f7d970a641d3465
Adam Dobrawy <naczelnik@jawnosc.tk>
2019-10-31 12:51:58 +0100

    Pass env INFLUXDB_* to container

--- buildTravis.sh
+++ buildTravis.sh
@@ -2,5 +2,5 @@
 set -eux
 cp ./resources/secrets/id_rsa.pub ./resources/ssh/id_rsa.pub;
 docker build . -t recommended-images;
-COMMON="-e H1_TOKEN=$H1_TOKEN -e RBX_TOKEN=$RBX_TOKEN -e SCOPE=$SCOPE";
+COMMON="-e H1_TOKEN=$H1_TOKEN -e RBX_TOKEN=$RBX_TOKEN -e SCOPE=$SCOPE -e INFLUXDB_HOST -e INFLUXDB_PASSWORD -e INFLUXDB_USER";
 docker run --rm -it $COMMON recommended-images nodejs buildTestPublish.js --config "$CONFIG" --mode "$MODE" --cleanup --publish
