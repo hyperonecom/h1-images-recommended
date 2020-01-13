bc952dc420ec99c23e7773c82101418777d37f85
Adam Dobrawy <naczelnik@jawnosc.tk>
2020-01-13 18:04:14 +0100

    Fix build Debian 8 on RBX due mtab missing

--- scripts/debian/debian-8-jessie.sh
+++ scripts/debian/debian-8-jessie.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -eux
+df -P .
 DEVICE=$(df -P . | awk 'END{print $1}')
 DEVICE_DISK=$(echo $DEVICE | sed 's/[0-9]//g' )
 mkdir /run/resolvconf
@@ -10,11 +11,11 @@ apt-get update && apt-get -y dist-upgrade
 apt-get -y install debconf-utils vim resolvconf arping curl
 echo "grub-pc grub-pc/install_devices string ${DEVICE_DISK}" | debconf-set-selections
 echo "grub-efi-amd64 grub2/force_efi_extra_removable boolean true" | debconf-set-selections
-apt-get install -y grub2-common grub-efi-amd64-bin grub-pc-bin
-sed -i 's/^GRUB_CMDLINE_LINUX=.*$/GRUB_CMDLINE_LINUX="elevator=noop consoleblank=0 console=tty0 console=ttyS0,115200n8"/' /etc/default/grub
-sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT=.*$/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/' /etc/default/grub;
+apt-get install -y grub2-common grub-efi-amd64-bin grub-pc
 grub-install --target=x86_64-efi --no-nvram --force-extra-removable
 grub-install --target=i386-pc ${DEVICE_DISK}
+sed -i 's/^GRUB_CMDLINE_LINUX=.*$/GRUB_CMDLINE_LINUX="elevator=noop consoleblank=0 console=tty0 console=ttyS0,115200n8"/' /etc/default/grub
+sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT=.*$/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/' /etc/default/grub;
 update-grub
 echo 'blacklist floppy' > /etc/modprobe.d/blacklist-floppy.conf
 update-initramfs -u
--- scripts/fstab.sh
+++ scripts/fstab.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+set -eux
+sudo ln -s /proc/self/mounts /etc/mtab;
 DEVICE=$(df -P . | awk 'END{print $1}')
 DISK_UUID=$(blkid -s UUID -o value "$DEVICE")
 echo "UUID=$DISK_UUID    /    ext4    defaults    1    1" > /etc/fstab
