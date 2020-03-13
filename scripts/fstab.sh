8985bd00bc2d8f53656348369047a65991e246db
Adam Dobrawy <naczelnik@jawnosc.tk>
2020-03-13 18:09:42 +0100

    Support non-ext4 in fstab

--- scripts/fstab.sh
+++ scripts/fstab.sh
@@ -4,4 +4,5 @@ rm -f /etc/mtab
 ln -s /proc/self/mounts /etc/mtab;
 DEVICE=$(df -P . | awk 'END{print $1}')
 DISK_UUID=$(blkid -s UUID -o value "$DEVICE")
-echo "UUID=$DISK_UUID    /    ext4    defaults    1    1" > /etc/fstab
+DISK_FS=$(blkid -s TYPE -o value "$DEVICE")
+echo "UUID=$DISK_UUID    /    $DISK_FS    defaults    1    1" > /etc/fstab
