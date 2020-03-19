8833d2c9f062f53ed35846d8725070216244e25d
Adam Dobrawy <naczelnik@jawnosc.tk>
2020-03-19 10:26:01 +0100

    Fix GPT resize for Debian 8

--- scripts/debian/debian-8-jessie.sh
+++ scripts/debian/debian-8-jessie.sh
@@ -3,11 +3,13 @@ set -eux
 df -P .
 DEVICE=$(df -P . | awk 'END{print $1}')
 DEVICE_DISK=$(echo $DEVICE | sed 's/[0-9]//g' )
-mkdir /run/resolvconf
+mkdir -p /run/resolvconf
 export DEBIAN_FRONTEND=noninteractive; 
 echo 'nameserver 9.9.9.9' > /etc/resolv.conf
 echo 'nameserver 8.8.8.8' >> /etc/resolv.conf
-apt-get update && apt-get -y dist-upgrade
+
+echo "deb [check-valid-until=no] http://archive.debian.org/debian jessie-backports main" > /etc/apt/sources.list.d/jessie-backports.list
+apt-get -o Acquire::Check-Valid-Until=false update && apt-get -o Acquire::Check-Valid-Until=false -y dist-upgrade
 apt-get -y install debconf-utils vim resolvconf arping curl
 echo "grub-pc grub-pc/install_devices string ${DEVICE_DISK}" | debconf-set-selections
 echo "grub-efi-amd64 grub2/force_efi_extra_removable boolean true" | debconf-set-selections
@@ -25,6 +27,14 @@ rm -r /home/debian/
 echo 'interface ignore wildcard' >> /etc/ntp.conf
 (echo 'source-directory interfaces.d'; echo 'source interfaces.d/*.cfg') > /etc/network/interfaces
 sed -i 's/^ForwardToConsole=.*$/ForwardToConsole=no/' /etc/systemd/journald.conf
+
+# Debian 8 is almost EOL, so repository system is not well-maintaned
+wget http://archive.debian.org/debian/pool/main/c/cloud-init/cloud-init_0.7.7~bzr1156-1~bpo8+1_all.deb -O /tmp/cloduinit.db
+wget http://archive.debian.org/debian/pool/main/c/cloud-utils/cloud-utils_0.29-1~bpo8+1_all.deb -O /tmp/cloud-utils.deb
+wget http://httpredir.debian.org/debian/pool/main/c/cdrkit/genisoimage_1.1.11-3_amd64.deb -O /tmp/geniso.deb
+wget http://httpredir.debian.org/debian/pool/main/liba/libaio/libaio1_0.3.110-1_amd64.deb -O /tmp/libaio.deb
+dpkg -i /tmp/*.deb
+apt-get -f install -y
 echo 'datasource_list: [ RbxCloud ]' > /etc/cloud/cloud.cfg.d/90_dpkg.cfg
 echo 'network: {config: disabled}' > /etc/cloud/cloud.cfg.d/99-disable-network-config.cfg
 rm /etc/hosts
