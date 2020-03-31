0774989a8e076e39467e7e9b82841b58eddbfbff
Adam Dobrawy <naczelnik@jawnosc.tk>
2020-03-31 18:15:16 +0200

    ass RHEL_SECRET to fix build RHEL

--- buildTravis.sh
+++ buildTravis.sh
@@ -2,5 +2,5 @@
 set -eux
 cp ./resources/secrets/id_rsa.pub ./resources/ssh/id_rsa.pub;
 docker build . -t recommended-images;
-COMMON="-e H1_TOKEN=$H1_TOKEN -e RBX_TOKEN=$RBX_TOKEN -e SCOPE=$SCOPE -e INFLUXDB_HOST -e INFLUXDB_PASSWORD -e INFLUXDB_USER";
+COMMON="-e H1_TOKEN -e RBX_TOKEN -e SCOPE -e INFLUXDB_HOST -e INFLUXDB_PASSWORD -e INFLUXDB_USER -e REDHAT_SECRET";
 docker run --rm -it $COMMON recommended-images nodejs buildTestPublish.js --config "$CONFIG" --mode "$MODE" --cleanup --publish
