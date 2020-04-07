443fe52d7027e3d3d499f597f70ce3dce8fa65cc
Adam Dobrawy <naczelnik@jawnosc.tk>
2020-04-07 11:02:08 +0200

    Update branch for Windows

--- resources/powershell/SetupWindowsImage.ps1
+++ resources/powershell/SetupWindowsImage.ps1
@@ -1,5 +1,5 @@
 param(
-  [String] $Repo = "https://raw.githubusercontent.com/hyperonecom/h1-images-recommended/windows/resources/powershell/",
+  [String] $Repo = "https://raw.githubusercontent.com/hyperonecom/h1-images-recommended/master/resources/powershell/",
   [String] $TempDir = "C:\Windows\Temp\",
   [String] $Logfile = "$($TempDir)SetupWindowsImage.log"
 )
