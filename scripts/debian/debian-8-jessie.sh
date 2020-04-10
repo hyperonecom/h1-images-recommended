b52451f823d5928bae6ff49386060a506b128555
Adam Dobrawy <naczelnik@jawnosc.tk>
2020-04-10 23:51:42 +0200

    Fix build for Debian 8

--- scripts/debian/debian-8-jessie.sh
+++ scripts/debian/debian-8-jessie.sh
@@ -29,12 +29,12 @@ sed -i 's/^ForwardToConsole=.*$/ForwardToConsole=no/' /etc/systemd/journald.conf
 
 # Debian 8 is almost EOL, so repository system is not well-maintaned
 rm /etc/cloud/cloud.cfg /etc/cloud/cloud.cfg.d/ -r
-wget http://archive.debian.org/debian/pool/main/c/cloud-init/cloud-init_0.7.7~bzr1156-1~bpo8+1_all.deb -O /tmp/cloduinit.deb
-wget http://archive.debian.org/debian/pool/main/c/cloud-utils/cloud-guest-utils_0.29-1~bpo8+1_all.deb -O /tmp/cloud-guest-utils.deb
-wget http://archive.debian.org/debian/pool/main/c/cloud-utils/cloud-utils_0.29-1~bpo8+1_all.deb -O /tmp/cloud-utils.deb
-wget http://archive.debian.org/debian/pool/main/c/cdrkit/genisoimage_1.1.11-3_amd64.deb -O /tmp/geniso.deb
-wget http://archive.debian.org/debian/pool/main/liba/libaio/libaio1_0.3.110-1_amd64.deb -O /tmp/libaio.deb
-wget http://archive.debian.org/debian/pool/main/c/cloud-utils/cloud-image-utils_0.29-1~bpo8+1_all.deb -o /tmp/cloud-image-utils.deb
+wget 'http://archive.debian.org/debian/pool/main/c/cloud-init/cloud-init_0.7.7~bzr1156-1~bpo8+1_all.deb' -O /tmp/cloduinit.deb
+wget 'http://archive.debian.org/debian/pool/main/c/cloud-utils/cloud-guest-utils_0.29-1~bpo8+1_all.deb' -O /tmp/cloud-guest-utils.deb
+wget 'http://archive.debian.org/debian/pool/main/c/cdrkit/genisoimage_1.1.11-3_amd64.deb' -O /tmp/geniso.deb
+wget 'http://archive.debian.org/debian/pool/main/liba/libaio/libaio1_0.3.110-1_amd64.deb' -O /tmp/libaio.deb
+wget 'http://archive.debian.org/debian/pool/main/c/cloud-utils/cloud-utils_0.29-1~bpo8+1_all.deb' -O /tmp/cloud-utils.deb
+wget 'http://archive.debian.org/debian/pool/main/c/cloud-utils/cloud-image-utils_0.29-1~bpo8+1_all.deb' -O /tmp/cloud-image-utils.deb
 dpkg --force-confnew -i /tmp/*.deb || apt-get -o Dpkg::Options::=--force-confnew  -f install -f -y
 echo 'datasource_list: [ RbxCloud ]' > /etc/cloud/cloud.cfg.d/90_dpkg.cfg
 echo 'network: {config: disabled}' > /etc/cloud/cloud.cfg.d/99-disable-network-config.cfg
