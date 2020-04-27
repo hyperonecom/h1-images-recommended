1bf5f37d96a7991eea917e13c8a74c607942e521
Adam Dobrawy <naczelnik@jawnosc.tk>
2020-04-28 01:19:57 +0200

    Suspend H1-CLI for RHEL

--- scripts/rhel/cli.sh
+++ scripts/rhel/cli.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eux
-SCOPE_CODE=$(echo "${SCOPE_NAME}" | tr '[:upper:]' '[:lower:]');
-. /etc/os-release
-VERSION_ID=$(echo $VERSION_ID | grep -E '^[0-9]+' -o)
-echo -e "[${SCOPE_CODE}_stable]\nname = ${SCOPE_NAME} - stable repository\nenabled = 1\nbaseurl = ${REPOSITORY}/linux/${ID}/${VERSION_ID}/stable/\ngpgcheck=1\ngpgkey=${REPOSITORY}/gpg.public.txt" > "/etc/yum.repos.d/${SCOPE_CODE}.repo";
-yum -y install "${CLI_PACKAGE}"
+# SCOPE_CODE=$(echo "${SCOPE_NAME}" | tr '[:upper:]' '[:lower:]');
+# . /etc/os-release
+# VERSION_ID=$(echo $VERSION_ID | grep -E '^[0-9]+' -o)
+# echo -e "[${SCOPE_CODE}_stable]\nname = ${SCOPE_NAME} - stable repository\nenabled = 1\nbaseurl = ${REPOSITORY}/linux/${ID}/${VERSION_ID}/stable/\ngpgcheck=1\ngpgkey=${REPOSITORY}/gpg.public.txt" > "/etc/yum.repos.d/${SCOPE_CODE}.repo";
+# yum -y install "${CLI_PACKAGE}"
