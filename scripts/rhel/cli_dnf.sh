33d93e557faf166a8988739d2609b3c1f6951ccf
Adam Dobrawy <naczelnik@jawnosc.tk>
2020-08-09 00:01:29 +0200

    Fix installation CLI for RHEL

---
+++ scripts/rhel/cli_dnf.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -eux
+SCOPE_CODE=$(echo "${SCOPE_NAME}" | tr '[:upper:]' '[:lower:]');
+. /etc/os-release;
+echo -e "[${SCOPE_CODE}_stable]\nname = ${SCOPE_NAME} - stable repository\nenabled = 1\nbaseurl = ${REPOSITORY}/linux/${ID}/${VERSION_ID/\.*/}/stable/\ngpgcheck=1\ngpgkey=${REPOSITORY}/gpg.public.txt" > "/etc/yum.repos.d/${SCOPE_CODE}.repo"
+yum makecache
+yum -y install "${CLI_PACKAGE}"
