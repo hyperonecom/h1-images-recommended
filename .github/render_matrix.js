d7bf23b6161a9ecc393157c882a48be81e8d09e0
Adam Dobrawy <naczelnik@jawnosc.tk>
2020-11-16 04:43:52 +0100

    Add FreeBSD 12.2 image and add flow PR

--- .github/render_matrix.js
+++ .github/render_matrix.js
@@ -1,16 +1,51 @@
 'use strict';
 const fs = require('fs');
+const util = require('util');
+const execFile = util.promisify(require('child_process').execFile);
 const { join } = require('path');
 const readDir = fs.promises.readdir;
 const readFile = fs.promises.readFile;
 const yaml = require('js-yaml');
+
+const GITHUB_OUTPUT = process.argv.includes('--github');
+const DIFF_MODE = process.argv.includes('--diff');
+const FAMILIES = ['alpine', 'centos', 'debian', 'rhel', 'ubuntu', 'freebsd', 'windows'];
+
+const hasChanged = async (paths) => {
+    try {
+        // Compare committed to master
+        await execFile('git', [
+            'diff',
+            '--quiet',
+            'master',
+            'HEAD',
+            '--',
+            ...paths,
+        ]);
+        // Compare uncommitted to HEAD
+        await execFile('git', [
+            'diff-index',
+            '--quiet',
+            'HEAD',
+            '--',
+            ...paths
+        ])
+        return false;
+    } catch (err) {
+        console.log(err);
+        return true;
+    }
+}
 
 const scope_list = ['h1', 'rbx'];
 
 const render = (templates) => {
     const include = [];
 
-    for (const [template, config] of Object.entries(templates)) {
+    for (const [template, config] of Object
+        .entries(templates)
+        .sort(([, a], [, b]) => (a.priority || 50) > (b.priority || 50))
+    ) {
         const scopes = config.scope || scope_list;
         for (const scope of scopes) {
             include.push({
@@ -25,26 +60,50 @@ const render = (templates) => {
     };
 };
 
-const renderMatrix = async (name) => {
+const renderMatrix = async (name, touched) => {
     const path = join(__dirname, './../config', name);
     const files = await readDir(path);
     const templates = {};
 
-    for (const template of files.reverse().filter(x => x.endsWith('.yaml'))) {
+    for (const template of files
+        .reverse()
+        .filter(x =>
+            x.endsWith('.yaml') &&
+            touched.some(y => x.startsWith(y))
+        )
+    ) {
         templates[`./config/${name}/${template}`] = yaml.safeLoad(await readFile(join(path, template)));
     }
-    if (process.argv.includes('--github')) {
+    if (GITHUB_OUTPUT) {
         console.log(`::set-output name=matrix-${name}::${JSON.stringify(render(templates))}`);
     } else {
         console.log(JSON.stringify(render(templates), null, 4));
     }
 }
 const main = async () => {
-    await renderMatrix('packer');
-    await renderMatrix('windows');
+    const touched = [];
+
+    for (const family of FAMILIES) {
+        let status = true;
+        if (DIFF_MODE) {
+            status = await hasChanged([
+                `./config/*/${family}-*`,
+                `./templates/*/${family}-*`,
+                `./scripts/${family}/*`,
+                `./resources/**`,
+            ]);
+        }
+        if (status) {
+            touched.push(family);
+        }
+    };
+    console.log('Families accepted:', touched);
+
+    await renderMatrix('packer', touched);
+    await renderMatrix('windows', touched);
 };
 
-main().then(console.log).catch(err => {
+main().catch(err => {
     console.error(err);
     process.exit(-1);
 });
