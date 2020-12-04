3fafe8753978d3fed95807a36d51cec498ffb82e
Adam Dobrawy <naczelnik@jawnosc.tk>
2020-12-04 16:49:10 +0100

    ubuntu: Resize disk for Desktop edition

--- scripts/check_df.sh
+++ scripts/check_df.sh
@@ -4,4 +4,7 @@
 USED=$(df / -P -a | awk 'FNR==2{print $3}')
 AVAILABLE=$(df / -P -a | awk 'FNR==2{print $4}')
 PCENT=$(( $USED*100/$AVAILABLE ))
+echo "Used space: $USED";
+echo "Available space: $AVAILABLE";
+echo "Percentage space: $PCENT";
 [ $PCENT -lt 90 ]
