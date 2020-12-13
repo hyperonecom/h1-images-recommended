47682944e8e8a6ecb06f8b021d742d5fb1fcebe9
Adam Dobrawy <naczelnik@jawnosc.tk>
2020-12-13 20:42:34 +0100

    Updata influxDB database names

--- .github/influx.sh
+++ .github/influx.sh
@@ -25,4 +25,4 @@ fi;
 
 tags=$(jq -nr --arg a "${GITHUB_REPOSITORY}" --arg b "${GITHUB_EVENT_NAME}" --arg c "${CONFIG}" --arg d "${SCOPE}" '[$a|@uri,$b|@uri,$c|@uri,$d|@uri] | join(",")')
 values=$(jq -nr --arg a "${GITHUB_SHA}" --arg b "${GITHUB_RUN_NUMBER}" --arg c "${INFLUXDB_VALUE}" '[$a|@uri,$b|@uri,$c|@uri] | join(",")')
-exec curl -XPOST "http://${INFLUXDB_USER}:${INFLUXDB_PASSWORD}@${INFLUXDB_HOST}/write?db=actions&precision=s" --data-raw "build,${tags} ${values} $ts"
+exec curl -XPOST "http://${INFLUXDB_USER}:${INFLUXDB_PASSWORD}@${INFLUXDB_HOST}/write?db=recommended_image&precision=s" --data-raw "build,${tags} ${values} ${ts}"
