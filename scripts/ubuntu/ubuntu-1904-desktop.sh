742f63e200223e50b21ca68e71ee882eb050e7d1
Adam Dobrawy <naczelnik@jawnosc.tk>
2020-12-30 15:19:56 +0100

    Disable avachi and cups

--- scripts/ubuntu/ubuntu-1904-desktop.sh
+++ scripts/ubuntu/ubuntu-1904-desktop.sh
@@ -2,3 +2,6 @@ apt-get purge ubuntu-release-upgrader-core python3-distupgrade;
 apt-get install -y ubuntu-desktop arping;
 rm /etc/network/if-pre-up.d/wpasupplicant || echo "No wpasupplicant file";
 systemctl set-default graphical
+systemctl disable avahi-daemon
+systemctl disable cups
+systemctl disable cups-browsed
