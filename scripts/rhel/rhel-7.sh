df7b5cbca04482ce56609bb654b1a2b75a2d755d
Adam Dobrawy <naczelnik@jawnosc.tk>
2020-12-31 01:11:17 +0100

    Disable LLM in RHEL

--- scripts/rhel/rhel-7.sh
+++ scripts/rhel/rhel-7.sh
@@ -7,6 +7,11 @@ systemctl disable systemd-readahead-replay.service
 systemctl disable tuned.service
 systemctl disable rhel-configure.service
 systemctl disable rhel-loadmodules.service
+systemctl disable rpcbind
+systemctl disable rpcbind.socket
+
+# disalbe LLM for systemd
+ sed -i -e 's/#LLMNR=yes/LLMNR=no/g' /etc/systemd/resolved.conf
 
 # Fix SELinux
 restorecon -vR / >> /dev/null && echo 'restorecon success' || echo 'restorecon failed'
