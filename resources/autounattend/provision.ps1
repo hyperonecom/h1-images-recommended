9d7e04618464bd9d86f90002f9fa5215d5106292
Adam Dobrawy <naczelnik@jawnosc.tk>
2021-02-22 04:35:22 +0100

    Set TLS protocol version for Windows

--- resources/autounattend/provision.ps1
+++ resources/autounattend/provision.ps1
@@ -1,2 +1,3 @@
 Start-Sleep -s 60;
+[Net.ServicePointManager]::SecurityProtocol = 'tls12, tls11, tls';
 iex(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/hyperonecom/h1-images-recommended/master/resources/powershell/SetupWindowsImage.ps1');
