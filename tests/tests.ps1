50716da44041f72c44674bb8cdd138b3a6ec1de2
Adam Dobrawy <naczelnik@jawnosc.tk>
2021-06-30 02:40:29 +0200

    disable cert validation in wsman

--- tests/tests.ps1
+++ tests/tests.ps1
@@ -4,6 +4,8 @@ param(
  [string]$User,
  [string]$Pass
 )
+Disable-WSManCertVerification -All # disable validation for WSMan - see: https://github.com/jborean93/omi/blob/main/docs/https_validation.md
+
 $pso = New-PSSessionOption -SkipCACheck -SkipCNCheck
 $secpasswd = ConvertTo-SecureString $Pass -AsPlainText -Force
 $creds = New-Object System.Management.Automation.PSCredential ($User, $secpasswd)
