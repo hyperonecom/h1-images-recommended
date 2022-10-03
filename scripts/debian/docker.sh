8f2bc9ca3e4972b368bcbf1e491ecefe5a2a3630
Frederico Silva <frederico.silva@gmail.com>
2022-10-03 17:14:49 +0200

    debian: docker, use docker-compose-plugin

--- scripts/debian/docker.sh
+++ scripts/debian/docker.sh
@@ -12,10 +12,4 @@ add-apt-repository \
    $(lsb_release -cs) \
    stable";
 apt-get update;
-apt-get install -y docker-ce docker-ce-cli containerd.io;
-# Docker-Compose
-COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases?per_page=1 | sed -E -n 's/^.*tag_name": "(.+?)".*$/\1/p')
-curl -L "https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose;
-chmod +x /usr/local/bin/docker-compose;
-# Docker-Compose (bash completion)
-curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin;
