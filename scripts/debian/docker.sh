d0d182f49440ba4d51082f655185ee555d78d49d
Gregory Brzeski <gregory@brzeski.pl>
2025-03-20 09:35:31 +0100

    Debian 12 Docker: new handling of apt keys

--- scripts/debian/docker.sh
+++ scripts/debian/docker.sh
@@ -5,11 +5,11 @@ apt-get install -y \
     ca-certificates \
     curl \
     gnupg2 \
-    software-properties-common;
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -;
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg > "/etc/apt/trusted.gpg.d/docker.asc"
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/debian \
    $(lsb_release -cs) \
-   stable";
-apt-get update;
-apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin;
+   stable"
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
